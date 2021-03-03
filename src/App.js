fb31c34c15caab84e1c083c5221434d956f3ad56
Leandro de Lima Coelho <leandrofreestyle18@gmail.com>
2021-03-02 22:19:43 -0300

    firt commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import NavBar from './components/NavBar'
+import Form from './components/Form';
+
+
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      
+    <NavBar></NavBar>
+    <br/>
+    <Form></Form>
+    </>
+
+    
+  );
+}
+
+export default App;
